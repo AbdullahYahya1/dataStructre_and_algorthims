5e8eae983cefa96a6f7538946e174ff0cc61c3f5
AbdullahYahya1 <al7rag50@gmail.com>
2023-07-31 02:57:00 +0300

    sol to https://leetcode.com/problems/car-fleet/

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # dataStructre_and_algorthims
+this is sol for 
+Given an array of integers temperatures represents the daily temperatures, return an array answer such that answer[i] is the number of days you have to wait after the ith day to get a warmer temperature. If there is no future day for which this is possible, keep answer[i] == 0 instead.
--- p1.py
+++ p1.py
@@ -1,17 +1,14 @@
 from typing import List
 
 class Solution:
-    def generateParenthesis(self, n: int) -> List[str]:
-        res = []
-        def backtrack(start=0, end=0 , s=''): 
-            if(end == n and start == n):
-                res.append(s)
-                return
-            if(start < n):
-                backtrack(start+1 , end , s+'(')
-            
-            if(end < start):
-                backtrack(start , end+1 , s+')')
-        backtrack()
-        return res 
-print( Solution().generateParenthesis(3))
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        counter = [0] * len(temperatures)
+        stack=[] #[i , temp]
+        for i , key in enumerate(temperatures):
+            while stack and stack[-1][1] < key:
+                index , temp = stack.pop()
+                length = i - index 
+                counter[index] = length
+            stack.append([i , key])
+        return counter 
+print(Solution().dailyTemperatures([1,2,0,2,4,5]))
