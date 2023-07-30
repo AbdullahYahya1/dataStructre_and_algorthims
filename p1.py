53b3528dc2123a5724a82e5a672a96d6d16b1de1
AbdullahYahya1 <al7rag50@gmail.com>
2023-07-30 06:32:14 +0300

    first program

---
+++ p1.py
@@ -0,0 +1,17 @@
+from typing import List
+
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        res = []
+        def backtrack(start=0, end=0 , s=''): 
+            if(end == n and start == n):
+                res.append(s)
+                return
+            if(start < n):
+                backtrack(start+1 , end , s+'(')
+            
+            if(end < start):
+                backtrack(start , end+1 , s+')')
+        backtrack()
+        return res 
+print( Solution().generateParenthesis(3))
