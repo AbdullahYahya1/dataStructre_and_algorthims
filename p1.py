8f4f1e5bbc205fb07510671cc528707908b63880
Abdullah <Abdullahyahyaalshami2001@gamil.com>
2023-08-02 15:09:40 +0300

    done

--- p1.py
+++ p1.py
@@ -1,14 +1,30 @@
 from typing import List
 class Solution:
-    def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        left = 0 
-        right = len(numbers)-1
-        while(left < right):
-            if(numbers[left] + numbers[right]  < target):
-                left+=1
-            elif(numbers[left] + numbers[right]  > target):
-                right-=1
-            else:
-                return [left+1 , right+1]
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums= sorted(nums)
+        res=[]
+        for i , a in enumerate(nums):
+            if a > 0:
+                break
+            if i>0 and a == nums[i-1]:
+                continue   
+            l , r = i+1 , len(nums)-1
+            while l < r: 
+                total = nums[l]  + nums[r] + a 
 
-print(Solution().twoSum([1,2,3,4,5,6,7],11))
+                if total > 0 : 
+                    r-=1
+                elif total < 0 : 
+                    l+=1
+                else: 
+                    print('here')
+                    res.append([ a , nums[l]  , nums[r]])
+                    l+=1 
+                    r-=1
+                    while nums[l] == nums[l - 1] and l < r:
+                        l += 1
+        return res
+print(
+Solution().threeSum([0,0,0,0]
+
+))
