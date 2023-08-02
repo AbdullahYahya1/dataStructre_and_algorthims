435d6e6216a65f4a674676b43947df1f7f783a10
Abdullah <Abdullahyahyaalshami2001@gamil.com>
2023-08-02 15:20:08 +0300

    max

--- p1.py
+++ p1.py
@@ -1,30 +1,18 @@
 from typing import List
 class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
-        nums= sorted(nums)
-        res=[]
-        for i , a in enumerate(nums):
-            if a > 0:
-                break
-            if i>0 and a == nums[i-1]:
-                continue   
-            l , r = i+1 , len(nums)-1
-            while l < r: 
-                total = nums[l]  + nums[r] + a 
-
-                if total > 0 : 
-                    r-=1
-                elif total < 0 : 
-                    l+=1
-                else: 
-                    print('here')
-                    res.append([ a , nums[l]  , nums[r]])
-                    l+=1 
-                    r-=1
-                    while nums[l] == nums[l - 1] and l < r:
-                        l += 1
-        return res
-print(
-Solution().threeSum([0,0,0,0]
-
-))
+    def maxArea(self, height: List[int]) -> int:
+        l = 0 
+        r = len(height)-1 
+        maxV = 0
+        while l < r:
+            d = r-l 
+            val=min( height[l] , height[r]) * d
+            print(val)
+            if maxV < val:
+                maxV = val 
+            if height[l] < height[r] :
+                l+=1 
+            elif height[l] >= height[r]:
+                r-=1 
+        return maxV
+print(Solution().maxArea([1,8,6,2,5,4,8,3,7]))
