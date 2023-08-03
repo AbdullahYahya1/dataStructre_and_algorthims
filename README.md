931ee519ba538be75890978f777260ed41b673e5
Abdullah <Abdullahyahyaalshami2001@gamil.com>
2023-08-03 19:12:58 +0300

    .

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # dataStructre_and_algorthims
+https://leetcode.com/problems/remove-nth-node-from-end-of-list/description/
--- p1.py
+++ p1.py
@@ -6,42 +6,14 @@ class ListNode:
         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        curr = head
-        while(n):
-            n-=1 
+        dummy_head = ListNode(next=head)
+        curr=head
+        while(n>0):
+            n-=1
             curr=curr.next
-            print(n)
-        print()
-        print()
-
+        pre =dummy_head
         while(curr):
-            print(curr.val)
             curr=curr.next
-        pass 
-
-            
-
-
-# Helper function to convert a list to a linked list
-def list_to_linked_list(lst):
-    dummy_head = ListNode()
-    current = dummy_head
-    for val in lst:
-        current.next = ListNode(val)
-        current = current.next
-    return dummy_head.next
-
-def linked_list_to_list(head):
-    result = []
-    while head:
-        result.append(head.val)
-        head = head.next
-    return result
-if __name__ == "__main__":
-    input_list = [1, 2, 3, 4, 5]
-    linked_list = list_to_linked_list(input_list)
-    solution = Solution()
-    solution.removeNthFromEnd(linked_list , n=1)
-    output_list = linked_list_to_list(linked_list)
-    print("Input List:", input_list)
-    print("Modified List:", output_list)
+            pre=pre.next
+        pre.next=pre.next.next
+        return dummy_head.next
