2310cc943f8554558f17fc03692cda0aba3deb5c
Abdullah <Abdullahyahyaalshami2001@gamil.com>
2023-08-03 22:39:39 +0300

    .

--- p1.py
+++ p1.py
@@ -1,24 +1,15 @@
 from typing import Optional
 class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 class Solution:
-    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        curr1 = l1
-        curr2 = l2
-        newList = ListNode()
-        currRes = newList
-        remindr = 0
-
-        while curr1 or curr2 or remindr:
-            val = (curr1.val if curr1 else 0) + (curr2.val if curr2 else 0) + remindr
-            remindr = val // 10
-            val = val % 10
-            currRes.next = ListNode(val)
-            curr1 = curr1.next if curr1 else None
-            curr2 = curr2.next if curr2 else None
-            currRes = currRes.next
-
-        return newList.next
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        seen=set()
+        curr= head
+        while curr : 
+            if curr in seen:
+                return True
+            seen.add(curr)
+            curr=curr.next
+        return False
