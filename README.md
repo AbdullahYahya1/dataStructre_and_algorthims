18989d7eda682e6335e1946e3f09fdf75d77fe56
Abdullah <Abdullahyahyaalshami2001@gamil.com>
2023-08-03 17:55:09 +0300

    .

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # dataStructre_and_algorthims
-https://leetcode.com/problems/merge-two-sorted-lists/
+https://leetcode.com/problems/reorder-list/description/
--- p1.py
+++ p1.py
@@ -1,23 +1,62 @@
 from typing import Optional
+import math
+
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
 class Solution:
-    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:    
-        sol = ListNode()
-        tail=sol
-        while list2 and list1:
-            if(list1.val < list2.val):
-                tail.next = list1
-                list1 = list1.next
-            else:
-                tail.next = list2
-                list2 = list2.next
-            tail = tail.next
-        if(list1):
-            tail.next = list1
-        elif list2:
-            tail.next = list2
-        
-        return sol.next
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        #SPLIT
+        slow , fast = head , head.next
+        while(fast and fast.next):
+            slow = slow.next
+            fast=fast.next.next 
+        half=slow.next
+        slow.next=None
+
+
+        #TAKE OPSIT 
+        curr , pre= half , None
+        while(curr):
+            swap=curr.next
+            curr.next=pre
+            pre=curr
+            curr=swap
+
+        #it is opesit and stored in half 
+        #now we will mearge them to make new arr 
+        curr , half = head , pre 
+        while(half):
+            swap = curr.next
+            swaph = half.next
+            curr.next=half
+            half.next= swap
+            curr=swap
+            half=swaph
+            
+
+
+# Helper function to convert a list to a linked list
+def list_to_linked_list(lst):
+    dummy_head = ListNode()
+    current = dummy_head
+    for val in lst:
+        current.next = ListNode(val)
+        current = current.next
+    return dummy_head.next
+
+def linked_list_to_list(head):
+    result = []
+    while head:
+        result.append(head.val)
+        head = head.next
+    return result
+if __name__ == "__main__":
+    input_list = [1, 2, 3, 4, 5]
+    linked_list = list_to_linked_list(input_list)
+    solution = Solution()
+    solution.reorderList(linked_list)
+    output_list = linked_list_to_list(linked_list)
+    print("Input List:", input_list)
+    print("Modified List:", output_list)
