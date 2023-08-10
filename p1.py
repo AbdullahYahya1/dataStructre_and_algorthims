7db68d7145ac774b8dfeee4414196e764bb9197e
AbdullahYahya1 <al7rag50@gmail.com>
2023-08-09 22:07:39 -0700

    what ever

--- p1.py
+++ p1.py
@@ -1,16 +1,6 @@
 from typing import Optional , List
 
 class Solution:
-<<<<<<< HEAD
-    def generateParenthesis(self, n: int) -> List[str]:
-        res = []
-        def backtrack(start=0, end=0 , s=''): 
-            if(end == n and start == n):
-                res.append(s)
-                return
-            if(start < n) :
-                backtrack(start+1 , end , s+'(')
-=======
     def findDuplicate(self, nums: List[int]) -> int:
         slow , fast = 0 ,  0 
         while True:
@@ -26,7 +16,6 @@ class Solution:
             slow= nums[slow]
             if slow2 == slow:
                 return slow 
->>>>>>> ed456ed668cae2089577914028e4c08a47902217
             
 
 print(Solution().findDuplicate([1,3,4,2,2]))
